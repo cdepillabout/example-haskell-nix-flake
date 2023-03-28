8c5d1de69f215e43fa224ea6ef9bf4e77de50630
Dennis Gosnell <cdep.illabout@gmail.com>
2023-03-29 08:13:12 +0900

    Get everything working and add a README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # example-haskell-nix-flake
+
+This is an example Nix Flake for building and developing a Haskell application.
+
+After cloning this repository, you should be able to build this Haskell package
+with the following command:
+
+```console
+$ nix build -L
+```
+
+You should be able to jump into a development shell with the following command:
+
+```console
+$ nix develop -L
+```
+
+From this development shell, you should be able to build the package with
+`cabal`:
+
+```console
+$ cabal build
+```
+
+From the development shell, you should also be able to run
+`haskell-language-server` to confirm that it can build all your Haskell
+modules:
+
+```console
+$ haskell-language-server
+...
+Completed (1 file worked, 0 files failed)
+```
--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Example Nix Flake for Haskell";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-utils = {
       url = "github:numtide/flake-utils";
@@ -15,34 +15,48 @@
       (system:
         let
           config = {};
+
           overlays = [
+            # This is an overlay we apply on top of Nixpkgs with some of our
+            # own packages defined.
             (final: prev: {
-              myHaskellPackages = final.haskell.packages.ghc924.override {
+              # A Haskell package set with our own overrides and packages defined.
+              myHaskellPackages = final.haskellPackages.override {
                 overrides = hfinal: hprev: {
-                  example-haskell-nix-flake = hfinal.callCabal2nix "example-haskell-nix-flake" ./. {};
+                  # This is our local Haskell package.
+                  example-haskell-nix-flake =
+                    hfinal.callCabal2nix "example-haskell-nix-flake" ./. {};
                 };
               };
 
+              # This is just a convenient shortcut to our package from the
+              # top-level of Nixpkgs.  We're also applying the
+              # justStaticExecutables function to our package in order to
+              # reduce the size of the output derivation.
               example-haskell-nix-flake =
                 final.haskell.lib.compose.justStaticExecutables
                   final.myHaskellPackages.example-haskell-nix-flake;
 
+              # A Haskell development shell for our package that includes
+              # things like cabal and HLS.
               myDevShell = final.myHaskellPackages.shellFor {
                 packages = p: [ p.example-haskell-nix-flake ];
 
                 nativeBuildInputs = [
                   final.cabal-install
-                  final.haskell.packages.ghc924.haskell-language-server
+                  final.haskellPackages.haskell-language-server
                 ];
               };
             })
           ];
+
+          # Our full Nixpkgs with the above overlay applied.
           pkgs = import nixpkgs { inherit config overlays system; };
         in
         {
           packages.default = pkgs.example-haskell-nix-flake;
 
-          # devShell.default = pkgs.myDevShell;
+          devShells.default = pkgs.myDevShell;
         }
       );
 }
