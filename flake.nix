769507698d369d95e250478e60e2b7e1d756537d
Nikodem Rabuliński <nikodem@rabulinski.com>
2023-10-08 21:58:15 +0200

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -3,60 +3,52 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { flake-utils, nixpkgs, self }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          config = {};
+  outputs = {
+    nixpkgs,
+    self,
+  }: let
+    overlay = final: prev: {
+      haskellPackages = prev.haskellPackages.override {
+        overrides = hfinal: hprev: {
+          example-haskell-nix-flake =
+            hfinal.callCabal2nix "example-haskell-nix-flake" ./. {};
+        };
+      };
 
-          overlays = [
-            # This is an overlay we apply on top of Nixpkgs with some of our
-            # own packages defined.
-            (final: prev: {
-              # A Haskell package set with our own overrides and packages defined.
-              myHaskellPackages = final.haskellPackages.override {
-                overrides = hfinal: hprev: {
-                  # This is our local Haskell package.
-                  example-haskell-nix-flake =
-                    hfinal.callCabal2nix "example-haskell-nix-flake" ./. {};
-                };
-              };
+      example-haskell-nix-flake =
+        final.haskell.lib.compose.justStaticExecutables
+        final.haskellPackages.example-haskell-nix-flake;
+    };
 
-              # This is just a convenient shortcut to our package from the
-              # top-level of Nixpkgs.  We're also applying the
-              # justStaticExecutables function to our package in order to
-              # reduce the size of the output derivation.
-              example-haskell-nix-flake =
-                final.haskell.lib.compose.justStaticExecutables
-                  final.myHaskellPackages.example-haskell-nix-flake;
+    forEachSystem = f:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ] (system:
+        f (import nixpkgs {
+          inherit system;
+          overlays = [overlay];
+        }));
+  in {
+    overlays.default = overlay;
 
-              # A Haskell development shell for our package that includes
-              # things like cabal and HLS.
-              myDevShell = final.myHaskellPackages.shellFor {
-                packages = p: [ p.example-haskell-nix-flake ];
+    packages = forEachSystem (pkgs: {
+      default = pkgs.example-haskell-nix-flake;
+    });
 
-                nativeBuildInputs = [
-                  final.cabal-install
-                  final.haskellPackages.haskell-language-server
-                ];
-              };
-            })
-          ];
+    devShells = forEachSystem (pkgs: {
+      default = pkgs.haskellPackages.shellFor {
+        packages = ps: [ps.example-haskell-nix-flake];
 
-          # Our full Nixpkgs with the above overlay applied.
-          pkgs = import nixpkgs { inherit config overlays system; };
-        in
-        {
-          packages.default = pkgs.example-haskell-nix-flake;
-
-          devShells.default = pkgs.myDevShell;
-        }
-      );
+        nativeBuildInputs = [
+          pkgs.cabal-install
+          pkgs.haskellPackages.haskell-language-server
+        ];
+      };
+    });
+  };
 }
