45beddfadc87ecb06c79a4422b116d6963c34672
Dennis Gosnell <cdep.illabout@gmail.com>
2023-10-18 08:00:08 +0900

    Reformat

--- flake.nix
+++ flake.nix
@@ -5,52 +5,49 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
   };
 
-  outputs = {
-    nixpkgs,
-    self,
-  }: let
-    overlay = final: prev: {
-      myHaskellPackages = prev.haskellPackages.override {
-        overrides = hfinal: hprev: {
-          example-haskell-nix-flake =
-            hfinal.callCabal2nix "example-haskell-nix-flake" ./. {};
+  outputs = { nixpkgs, self, }:
+    let
+      overlay = final: prev: {
+        myHaskellPackages = prev.haskellPackages.override {
+          overrides = hfinal: hprev: {
+            example-haskell-nix-flake =
+              hfinal.callCabal2nix "example-haskell-nix-flake" ./. {};
+          };
+        };
+
+        example-haskell-nix-flake =
+          final.haskell.lib.compose.justStaticExecutables
+            final.myHaskellPackages.example-haskell-nix-flake;
+
+        my-dev-shell = final.myHaskellPackages.shellFor {
+          packages = ps: [ps.example-haskell-nix-flake];
+
+          nativeBuildInputs = [
+            final.cabal-install
+            final.haskellPackages.haskell-language-server
+          ];
         };
       };
 
-      example-haskell-nix-flake =
-        final.haskell.lib.compose.justStaticExecutables
-        final.myHaskellPackages.example-haskell-nix-flake;
+      forEachSystem = f:
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+          (system: f (import nixpkgs { inherit system; overlays = [overlay]; }));
+    in
+    {
+      overlays.default = overlay;
 
-      my-dev-shell = final.myHaskellPackages.shellFor {
-        packages = ps: [ps.example-haskell-nix-flake];
+      packages = forEachSystem (pkgs: {
+        default = pkgs.example-haskell-nix-flake;
+      });
 
-        nativeBuildInputs = [
-          final.cabal-install
-          final.haskellPackages.haskell-language-server
-        ];
-      };
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.my-dev-shell;
+      });
     };
-
-    forEachSystem = f:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ] (system:
-        f (import nixpkgs {
-          inherit system;
-          overlays = [overlay];
-        }));
-  in {
-    overlays.default = overlay;
-
-    packages = forEachSystem (pkgs: {
-      default = pkgs.example-haskell-nix-flake;
-    });
-
-    devShells = forEachSystem (pkgs: {
-      default = pkgs.my-dev-shell;
-    });
-  };
 }
